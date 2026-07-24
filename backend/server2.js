dd3307dc0cfbdf63dce1a213874203c67e77c2e1
Chandan0Singh <code.chandansingh@gmail.com>
2026-07-25 05:09:33 +0530

    .

--- backend/server2.js
+++ backend/server2.js
@@ -2,14 +2,16 @@ const http = require("http");
 
 const PORT = process.env.PORT || 8080;
 
-console.log("PORT =", PORT);
-
 const server = http.createServer((req, res) => {
   console.log("Request:", req.url);
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello");
+
+  res.writeHead(200, {
+    "Content-Type": "text/plain",
+  });
+
+  res.end("Railway Working");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log(server.address());
+  console.log("Listening", PORT);
 });
