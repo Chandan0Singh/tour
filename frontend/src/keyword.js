26ac7bb69d86d7ebb492f3d49221b04b1c65eae7
Chandan0Singh <code.chandansingh@gmail.com>
2026-08-30 18:14:31 +0530

    check

--- frontend/src/keyword.js
+++ frontend/src/keyword.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = process.env.API_URL || "http://localhost:5000";
-export const BACKEND_URL = process.env.API_URL || "https://tour-4exy.onrender.com";
+// export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "https://tour-4exy.onrender.com";
