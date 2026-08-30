b1d1fe8ce426ac480b9c58748555fa1db93d851f
Chandan0Singh <code.chandansingh@gmail.com>
2026-08-30 17:22:32 +0530

    console. debugging

--- frontend/src/app/Components/Header.jsx
+++ frontend/src/app/Components/Header.jsx
@@ -131,6 +131,8 @@ export default function Header() {
 
         const data = await response.json();
 
+        console.log("data :", data)
+
         if (data.success) {
           setMenu(data.menu);
         }
